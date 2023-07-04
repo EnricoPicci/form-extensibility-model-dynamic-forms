04ff82ced126ba93909e0ef7ee69dede4a8b2e79
Enrico Piccinin <enrico.piccinin@gmail.com>
2023-07-04 11:38:09 +0200

    project ngx-dynami-forms deleted

--- projects/ngx-dynamic-forms/src/public-api.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Public API Surface of ngx-dynamic-forms
- */
-
-export * from './lib/form-routes';
-export * from './lib/dynamic-form/dynamic-form.component';
-export * from './lib/dynamic-form-question/dynamic-form-question.component';
-export * from './lib/dynamic-form-section/dynamic-form-section.component';
-export * from './lib/ngx-dynamic-forms.module';
